9a1855a7bd614ecac000520471b6d6b272d7db27
sandroh <sandroh.luiz@gmail.com>
2024-08-14 22:21:34 -0300

    aulas 02 e 03

--- dio/operadores/Operadores.java
+++ dio/operadores/Operadores.java
@@ -4,4 +4,38 @@ public class Operadores {
   public static void main(String[] args) {
     
   }
+/*
+02 Operadores
+  Operadores de atribuição
+    Usado para definir um valor inicial ou sobrescrever o valor de uma var
+    String nome = "Sandroh";
+    int idade = 22;
+    double peso = 64.4;
+    char sexo = 'M';
+    boolean doadorOrgao = false;
+    Date dataNascimento = new Date(); //Classe
+  Operadores aritméticos
+    Realiza operações matemáticas
+    double soma = 10.5 + 15.7;
+    int subtracao = 10 - 25;
+    int multiplicacao = 3 + 2;
+    int divisao = 15 / 3;
+    int modulo = 18 % 3; Resto de uma divisão
+    O operador de adição +, quando usado em var do tipo texto,
+    realizará a concatenação de textos.
+    ex: String nomeCompleto = "Sandro" + "Costa";
+03 Operadores unários
+  Esses operadores são aplicados juntamente com outro operador
+  aritmético. Eles realizam alguns trabalhos básicos como incrementar,
+  decrementar, inveter valores numéricos e booleanos.
+  (+) Unário de valor positivo
+    numero = + numero;
+    numero = numero * -1; Caso o numero que queira converter já seja negativo
+  (-) Unário de valor negativo
+    numero = - numero;
+  (++) Unário de incremento de valor
+  (--) Unário de decremento de valor
+  (!) Unário de negação de valor
+
+*/
 }

13e96fc8515d6d4d217d26f92004b896455003c0
sandroh <sandroh.luiz@gmail.com>
2024-08-16 15:48:27 -0300

    aula escopo

---
+++ dio/escopo/Escopo.java
@@ -0,0 +1,12 @@
+package dio.escopo;
+
+public class Escopo {
+  public static void main(String[] args) {
+    
+  }
+/*
+02 Escopo
+  Escopo é um ambiente onde uma variável pode ser acessada
+
+*/
+}

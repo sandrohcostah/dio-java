7ac51cf902db5b2b194456883a613edfb3fea2be
sandroh <sandroh.luiz@gmail.com>
2024-08-21 11:21:15 -0300

    condicional Simples

--- dio/EstruturasCondicionais/EstruturasCondicionais.java
+++ dio/EstruturasCondicionais/EstruturasCondicionais.java
@@ -4,4 +4,15 @@ public class EstruturasCondicionais {
     public static void main(String[] args) {
       
     }
+/*
+Estruturas Condicionais
+  if-else, switch-case
+Estruturas de repetição
+  for, while, do-while
+Estruturas de exceções
+  try-catch-finally
+
+Condicional Simples
+  Só há dois fluxos, true ou false
+*/
 }

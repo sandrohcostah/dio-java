1fcdb6ba2b936ce010ecbb78a5941c12f22d4101
sandroh <sandroh.luiz@gmail.com>
2024-08-22 09:44:47 -0300

    try/catch

---
+++ dio/EstruturasExcepcionais/EstruturasExcepcionais.java
@@ -0,0 +1,31 @@
+package dio.EstruturasExcepcionais;
+
+public class EstruturasExcepcionais{
+  public static void main(String[] args) {
+    
+  }
+/*
+Tratamento de exceções
+  Exceções são um fluxo não pevisto pela aplicação,
+  que evita a aplição parar
+  Dev precisa prever esses casos e fazer o tratamento de exceções
+  Try =
+    bloco de código que vai ser testado
+  Catch =
+    define o bloco de código a ser executado,
+  caso ocorra o erro no bloco try
+  Finally =
+    Define um código a ser excutado independente de ocorrer erro ou não
+    Esse é opcional
+  
+  Try {
+    código conforme esperado
+  }
+    catch(Exception e){
+      Precisamos saber qual a exceção
+      Bloco captura  as exceções que podem acontecer
+      Em caso de um fluxo não previsto
+    }
+  
+*/
+}

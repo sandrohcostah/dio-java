bcb2d521fe9e122095bbf9f664990b0d8b9c9cd0
sandroh <sandroh.luiz@gmail.com>
2024-08-23 10:14:02 -0300

    processo seletivo

--- dio/ValidacaoProcessoSeletivo/ProcessoSeletivo.java
+++ dio/ValidacaoProcessoSeletivo/ProcessoSeletivo.java
@@ -1,9 +1,52 @@
 package dio.ValidacaoProcessoSeletivo;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
   public static void main(String[] args) {
-    analisarCandidato(1900.0);
-    analisarCandidato(2200.0);
+    imprimirSelecionados();
+    //analisarCandidato(1900.0);
+    //analisarCandidato(2200.0);
+  }
+
+  static void imprimirSelecionados(){
+    String [] candidatos = {"Felipe","Marcia","Julia","Paulo","Augusto"};
+    //Imprimi lista informando o indice do elemento
+    System.out.println("Imprimindo a lista de candidatos");
+
+    for(int indice=0; indice < candidatos.length; indice++){
+      System.out.println("O candidato de nº " +(indice+1)+ " é " +candidatos[indice]);
+    }
+    //forma abreviada for each
+    //nesse caso não tem o indice
+    for(String candidato: candidatos){
+      System.out.println("O candidato selecionado foi " + candidato);
+    }
+
+  }
+
+  static void selecaoCandidatos(){
+    String [] candidatos = {"Felipe","Marcia","Julia","Paulo","Augusto","Monica","Fabricio","Mirela","Daniela","Jorge"};
+
+    int candidatosSelecionados = 0;
+    int candidatoAtual=0;
+    double salarioBase=2000.0;
+
+    while(candidatosSelecionados < 5 && candidatoAtual < candidatos.length){
+      String candidato = candidatos[candidatoAtual];
+      double salarioPretenido = valorPretendido();
+
+      System.out.println("O candidato " + candidato + " Solicitou este valor de salário " + salarioPretenido);
+      if(salarioBase >= salarioPretenido){
+        System.out.println("O candidato " +candidato+ " foi selecionado para a vaga");
+        candidatosSelecionados++;
+      }
+      candidatoAtual++;
+
+    }
+  }
+
+  static double valorPretendido(){
+    return ThreadLocalRandom.current().nextDouble(1800, 2200);
   }
 
   static void analisarCandidato(double salarioPretenido){

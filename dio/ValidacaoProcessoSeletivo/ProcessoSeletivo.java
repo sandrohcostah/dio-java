e405cb07a1b03b72cd87029ceac2ffc902a44fe5
sandroh <sandroh.luiz@gmail.com>
2024-08-23 10:29:05 -0300

    ProcessoSeletivo completo

--- dio/ValidacaoProcessoSeletivo/ProcessoSeletivo.java
+++ dio/ValidacaoProcessoSeletivo/ProcessoSeletivo.java
@@ -1,11 +1,44 @@
 package dio.ValidacaoProcessoSeletivo;
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
   public static void main(String[] args) {
-    imprimirSelecionados();
+    String [] candidatos = {"Felipe","Marcia","Julia","Paulo","Augusto"};
+    for(String candidato: candidatos){
+      entrandoEmContato(candidato);
+    }
+    //imprimirSelecionados();
     //analisarCandidato(1900.0);
     //analisarCandidato(2200.0);
+  }
+
+  static void entrandoEmContato(String candidato){
+    int tentativasRealizadas = 1;
+    boolean continuarTentando = true;
+    boolean atendeu=false;
+
+    do {
+      atendeu = atender();
+      continuarTentando = !atendeu;
+      if(continuarTentando){
+        tentativasRealizadas++;
+      }else{
+        System.out.println("Contato realizado com sucesso");
+      }
+    }while(continuarTentando && tentativasRealizadas<3);
+
+    if(atendeu){
+      System.out.println("Conseguimos contato com " +candidato+
+      " na " + tentativasRealizadas +" Tentativa");
+    } else {
+      System.out.println("Não conseguimos contato com " +candidato+
+      "número máximo de tentativas" +tentativasRealizadas+ " Realizadas");
+    }
+  }
+
+  static boolean atender(){
+    return new Random().nextInt(3)==1;
   }
 
   static void imprimirSelecionados(){

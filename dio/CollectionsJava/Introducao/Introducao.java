dc5a7e5e9f2dc3d3e09e012151d3f9ee3d6f13ee
sandroh <sandroh.luiz@gmail.com>
2024-09-03 20:58:14 -0300

    Conhecendo a hierarquia

---
+++ dio/CollectionsJava/Introducao/Introducao.java
@@ -0,0 +1,13 @@
+package dio.CollectionsJava.Introducao;
+
+public class Introducao {
+/*
+Um collection é uma estrutura de dados que serve para agrupar muitos elementos
+em uma única unidade, estes elementos precisam ser objetos
+Quatro tipos de coleções
+  List, Queue, Set e Map
+  Todas estão dentro do pacote java.util
+
+
+*/
+}

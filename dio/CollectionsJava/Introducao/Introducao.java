64417a227bfda44bd07660fc74311727026c3fad
sandroh <sandroh.luiz@gmail.com>
2024-09-04 21:13:41 -0300

    Entendendo o generics type

--- dio/CollectionsJava/Introducao/Introducao.java
+++ dio/CollectionsJava/Introducao/Introducao.java
@@ -8,6 +8,40 @@ Quatro tipos de coleções
   List, Queue, Set e Map
   Todas estão dentro do pacote java.util
 
+Entenden o generics Type
+  public class Box{
+    private Object object;
 
+    public void set(Object object){
+      this.object = object;
+    }
+    public Object get(){
+      return object;
+    }
+  }
+  <> diamond operator
+  Você cria uma declaração do tipo genérico, alterando o código
+  "public class Box" para "public class Box<T>"
+  public class Box<T>{ //T representa o tipo
+    private T t;
+
+    public void set(T t){
+      this.t = t;
+    }
+    public T get(){
+      return t;
+    }
+  }
+  Todas ocorrências de object foram substituidas por t
+  Uma variável de tipo pode ser qualquer tipo não primitivo que você quiser.
+  Qualquer tipo de classe, interface, array ou até mesmo outra variável de tipo.
+  Nomes mais usados:
+    E = elemento
+    K = Chave
+    N = número
+    T = Tipo
+    V = valor
+    S, U, etc
+  
 */
 }

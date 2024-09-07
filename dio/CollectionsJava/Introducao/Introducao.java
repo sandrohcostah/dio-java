be3c1b4123d334ca5092548ad443a09a113e73c6
sandroh <sandroh.luiz@gmail.com>
2024-09-07 18:47:23 -0300

    Comparable/comparator

--- dio/CollectionsJava/Introducao/Introducao.java
+++ dio/CollectionsJava/Introducao/Introducao.java
@@ -42,6 +42,22 @@ Entenden o generics Type
     T = Tipo
     V = valor
     S, U, etc
-  
-*/
+
+Conhecendo as interfaces comparable e comparator
+  Comparable
+    Usada para ser comparações, colocar uma lista em ordem alfabética...
+    Comparable está dentro do pacote java.lang
+    Fornece o método compareto()
+    Colocar os elementos de uma lista em ordem usando collections.sort(lista)
+  Comparator
+    Oferece o método compare()
+    Fornece multiplas sequências de ordenação. Pode ordenar por varios itens
+    Comparator está no pacote java.util
+    Colocar elementos em ordem em uma lista Collections.sort(Lista, Comparator)
+  Collections
+    É uma classe utilitária do java para operações comuns em coleções
+    Ela fornece métodos para ordenação, busca, manipulação e sincronização de coleções
+    O método sort() é usado para ordenar uma lista em ordem ascendente
+
+    */
 }

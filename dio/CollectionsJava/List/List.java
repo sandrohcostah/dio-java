85755c400b3d8ec663056145c09f07136a589c25
sandroh <sandroh.luiz@gmail.com>
2024-09-07 19:08:58 -0300

    List Interface

--- dio/CollectionsJava/List/List.java
+++ dio/CollectionsJava/List/List.java
@@ -3,6 +3,17 @@ package dio.CollectionsJava.List;
 public class List {
 /*
 List Interface
+  Uma das coleções mais usadas no java
+  As classes de implementação mais comuns são ArrayList e LinkedList
+  List se assemelha a uma matriz de comprimento dinâmico,
+    Permitindo adicionar ou remover elementos.
+  A Classe Collections fornece algoritmos uteis para manipulação de List,
+    Como ordenação (sort),
+    embaralhamento(shuffle), 
+    reversão (reverse) e 
+    busca binária(binarySearch)
+
+Iniciando o projeto e enviando para o git
   
 
 */

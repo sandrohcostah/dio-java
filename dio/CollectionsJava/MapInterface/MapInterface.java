1677ad6f8433a2044de5bdd0057194bdfbe89eca
sandroh <sandroh.luiz@gmail.com>
2024-09-14 21:05:05 -0300

    MapInterface

---
+++ dio/CollectionsJava/MapInterface/MapInterface.java
@@ -0,0 +1,18 @@
+package dio.CollectionsJava.MapInterface;
+
+public class MapInterface {
+/*
+A interface Map é usada para mmapear dados na forma de chaves e valores
+Map não pode ter chaves duplicadas
+Cada chave pode mapear para no máximo um valor
+Oprações básicas do Map:
+  put(insere)
+  get(obtem)
+  containsKey(Verifica se contém uma chave)
+  containsValue(verifica se contém um valor)
+  size(obter tamanho)
+  isEmpty(verifica se está vazio)
+  
+
+*/
+}

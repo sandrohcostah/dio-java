6a98bf0c2accc9b1af8f7b10923c3d74a2e3667e
sandroh <sandroh.luiz@gmail.com>
2024-10-05 18:06:16 -0300

    TesteDeSoftware

---
+++ dio/TesteDeSoftware/TesteDeSoftware.java
@@ -0,0 +1,60 @@
+package dio.TesteDeSoftware;
+
+public class TesteDeSoftware {
+/*
+Definições
+  É o processo de execução de um produto para determinar se ele atingiu suas especificações
+  Testes são usados para mostrar a presena de bugs, mas nunca para mostrar a sua ausência.
+
+Conceitos básicos
+  Defeito é a causa raiz
+  Erro é quando o bug acontece
+  Falha é quando o usuário vê
+
+Niveis de teste
+  São as fases de teste do software
+  Unidade = feito no inicio do teste de software
+  Integração = Faz teste com outros sistemas, integração com bd é um exmplo
+  Sistema = disponibiliza o sistema para testes em banco de produção
+  Regressão = reavaliar tudo que está sendo desenvolvido
+    Teste Alpha = Teste bem inicial, dando oportunidade para as pessoas avaliarem
+    Teste Beta = É para uma gama maior de usuários, mais amplo
+    Teste Cannary = é liberado uma versão diferente para algumas pessoas apenas, e ficam rodando duas versões em produção
+  Aceitação = momento em que a demanda já foi pra produção, e o cliente faz um pente final, uma avaliação do resultado
+
+Técnicas de teste
+  Caixa branca
+    Também conhecido como teste estrutural
+    Conhecer o código fonte
+    Garantir a qualidade da aplicação
+  Caixa preta
+    Teste funcional
+    Não se conhece a estrutura interna do software
+    Testa as saidas usando vários tipos de entrada
+  Caixa cinza
+    Mescla das duas anteriores
+    Analisa parte lógica e também a funcionalidade
+
+Testes não funcionais
+  Estão ligados a requisitos não funcionais
+    Comportamento do sistema, performance, escalabilidade, segurança, infraestrutura
+  Tem como objetivo testar aspectos do software que não são associados as regras de negócio mas sim a requisitos não funcionais
+  Teste de carga
+    Realizado para verificar qual o volume de transações, acessos simultâneos ou usuários que um servidor/software/sistema suporta
+    Ajuda a ter noção de como o sistema vai se comportar no mundo real
+    Gatting / apache jmeter
+  Teste de stress
+    Realizado para submeter o software a situaç~eos extremas.
+    Testa os limites do software
+
+Pirâmide de testes
+  São os vários niveis de testes
+  Base = testes de unidade
+  Meio = Testes de serviços
+  Topo = Teste de sistema
+  Pensar na estratégia de testes com base na pirâmide ajuda em relação e custos e tempo
+
+  
+
+*/
+}

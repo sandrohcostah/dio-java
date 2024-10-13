b4414b59509365fd618b172338893f366c60e0a9
sandroh <sandroh.luiz@gmail.com>
2024-10-13 20:35:39 -0300

    UsandoMockito

---
+++ dio/TesteDeSoftware/TestesUsandoMockito/UsandoMockito.java
@@ -0,0 +1,10 @@
+package dio.TesteDeSoftware.TestesUsandoMockito;
+
+public class UsandoMockito {
+/*
+O que são Mocks?
+  Validar se informação foi chamada no mock
+  Simular erros
+  
+*/
+}
---
+++ dio/TesteDeSoftware/TestesUsandoMockito/usandomockito/src/test/java/com/example/ListaTeste.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class ListaTeste {
+  
+  @Mock
+  private List<String> letras;
+
+  @Test
+  void adicionarItemNaLista(){
+    Mockito.when(letras.get(0)).thenReturn('A');
+   Assertions.assertEquals('A', letras.get(0)); 
+  }
+}
